9f7cee81c8635c000239e46b20ea247fa0f8e2e0
Paulo Hernane <paulohernane10@gmail.com>
2023-07-09 21:14:43 -0300

    styles(home): fix styles to mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,17 @@ import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
-  // ! The CSS is Broken
   return (
-    <header className={styles.heroBanner}>
-      <div className="">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
+    <header className={styles.banner}>
       <div>
-        <img
-          src="https://avatars.githubusercontent.com/u/36555768?v=4"
-          alt="me"
-        />
+        <h1>{siteConfig.title}</h1>
+        <p>{siteConfig.tagline}</p>
       </div>
+      <img
+        className={styles.banner__avatar}
+        src="https://avatars.githubusercontent.com/u/36555768?v=4"
+        alt="me"
+      />
     </header>
   );
 }
